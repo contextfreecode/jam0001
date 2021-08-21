6754f74e327c6310c545cc90711d0e2e2b65663c
Tom <tom@contextfree.info>
2021-08-20 19:14:13 -0700

    Start walking trees

--- ajo/ajo/main.py
+++ ajo/ajo/main.py
@@ -2,6 +2,10 @@ import argparse
 import ast
 
 
+def dump(node: ast.AST):
+    print(ast.dump(node, indent=4))
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--input")
@@ -9,8 +13,32 @@ def main():
     run(**args.__dict__)
 
 
+def process_class_field(node: ast.AnnAssign):
+    match node:
+        case ast.AnnAssign(annotation, target):
+            print("annotation")
+            dump(annotation)
+            print("target")
+            dump(target)
+
+
+def process_class_def(node: ast.ClassDef):
+    for kid in node.body:
+        match kid:
+            case ast.AnnAssign():
+                process_class_field(kid)
+
+
+def process_module(node: ast.Module):
+    # dump(node)
+    for kid in node.body:
+        match kid:
+            case ast.ClassDef():
+                process_class_def(kid)
+
+
 def run(*, input: str):
     with open(input) as input_stream:
         text = input_stream.read()
     tree = ast.parse(text)
-    print(ast.dump(tree, indent=4))
+    process_module(tree)
