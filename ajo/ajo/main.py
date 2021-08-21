364c3cb5ecd402bd845189fda84f97c55fe460b0
Tom <tom@contextfree.info>
2021-08-20 21:06:15 -0700

    Support default var type

--- ajo/ajo/main.py
+++ ajo/ajo/main.py
@@ -1,10 +1,20 @@
 import argparse
 import ast
+import dataclasses as dc
 import logging
 import textwrap
 
 
 logger = logging.getLogger(__name__)
+
+
+Key = str
+Type = str
+
+
+@dc.dataclass
+class Context:
+    defaults: dict[Key, Type]
 
 
 def dump(node: ast.AST):
@@ -19,36 +29,58 @@ def main():
     run(**args.__dict__)
 
 
-def process_class_field(node: ast.AnnAssign):
+def process_class_field(*, context: Context, node: ast.AnnAssign):
     target: ast.Name = node.target
     annotation: ast.Name = node.annotation
-    logging.info(f"{dump(annotation)=}")
-    logging.info(f"{dump(target)=}")
+    logger.debug(f"{dump(annotation)=}")
+    logger.debug(f"{dump(target)=}")
     print(f"    {target.id}: {annotation.id},")
 
 
-def process_class_def(node: ast.ClassDef):
+def process_class_def(*, context: Context, node: ast.ClassDef):
     for kid in node.body:
         match kid:
             case ast.AnnAssign():
-                process_class_field(kid)
+                process_class_field(context=context, node=kid)
+            case ast.Expr(value):
+                assert isinstance(value, ast.Name)
+                ann_assign = ast.AnnAssign(
+                    annotation=ast.Name(context.defaults[value.id]),
+                    target=value,
+                )
+                process_class_field(context=context, node=ann_assign)
+            case _:
+                logger.warning(f"unhandled: {dump(kid)}")
 
 
-def process_module(node: ast.Module):
+def process_module(*, context: Context, node: ast.Module):
     # dump(node)
     for kid in node.body:
         match kid:
             case ast.ClassDef():
                 print(f"const {kid.name} = struct {{")
-                process_class_def(kid)
+                process_class_def(context=context, node=kid)
                 print(f"}};")
+            case ast.AnnAssign():
+                process_var_default(context=context, node=kid)
+            case ast.Expr():
+                logger.debug(f"unhandled expr: {dump(kid)}")
+            case _:
+                logger.warning(f"unhandled: {dump(kid)}")
+
+
+def process_var_default(*, context: Context, node: ast.AnnAssign):
+    target: ast.Name = node.target
+    annotation: ast.Name = node.annotation
+    context.defaults[target.id] = annotation.id
 
 
 def run(*, input: str):
     with open(input) as input_stream:
         text = input_stream.read()
     tree = ast.parse(text)
-    process_module(tree)
+    context = Context(defaults={})
+    process_module(context=context, node=tree)
     script = f"""
         const std = @import("std");
         pub fn main() void {{
--- ajo/test/basic.zig
+++ ajo/test/basic.zig
@@ -2,6 +2,9 @@ const Person = struct {
     name: str,
     age: int,
 };
+const Car = struct {
+    age: int,
+};
 const std = @import("std");
 pub fn main() void {
     std.debug.print("Hi!\n", .{});
