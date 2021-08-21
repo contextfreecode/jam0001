4de45e823bc0becdae92d3407811d208883ced12
Tom <tom@contextfree.info>
2021-08-20 21:43:41 -0700

    Insert default docs

--- ajo/ajo/main.py
+++ ajo/ajo/main.py
@@ -8,13 +8,22 @@ import textwrap
 logger = logging.getLogger(__name__)
 
 
+Doc = str
 Key = str
 Type = str
 
 
 @dc.dataclass
 class Context:
-    defaults: dict[Key, Type]
+    default_docs: dict[Key, Doc]
+    default_types: dict[Key, Type]
+
+
+@dc.dataclass
+class VarDef:
+    annotation: ast.Name
+    doc: str
+    target: ast.Name
 
 
 def dump(node: ast.AST):
@@ -34,18 +43,21 @@ def process_class_field(*, context: Context, node: ast.AnnAssign):
     annotation: ast.Name = node.annotation
     logger.debug(f"{dump(annotation)=}")
     logger.debug(f"{dump(target)=}")
+    doc = context.default_docs.get(target.id)
+    if doc is not None:
+        print(f"    /// {doc}")
     print(f"    {target.id}: {annotation.id},")
 
 
 def process_class_def(*, context: Context, node: ast.ClassDef):
-    for kid in node.body:
+    for kid_index, kid in enumerate(node.body):
         match kid:
             case ast.AnnAssign():
                 process_class_field(context=context, node=kid)
             case ast.Expr(value):
                 assert isinstance(value, ast.Name)
                 ann_assign = ast.AnnAssign(
-                    annotation=ast.Name(context.defaults[value.id]),
+                    annotation=ast.Name(context.default_types[value.id]),
                     target=value,
                 )
                 process_class_field(context=context, node=ann_assign)
@@ -55,31 +67,50 @@ def process_class_def(*, context: Context, node: ast.ClassDef):
 
 def process_module(*, context: Context, node: ast.Module):
     # dump(node)
-    for kid in node.body:
+    doc = None
+    for kid_index, kid in enumerate(node.body):
         match kid:
             case ast.ClassDef():
                 print(f"const {kid.name} = struct {{")
                 process_class_def(context=context, node=kid)
                 print(f"}};")
             case ast.AnnAssign():
-                process_var_default(context=context, node=kid)
+                doc = None
+                if kid_index < len(node.body) - 1:
+                    match node.body[kid_index + 1]:
+                        case ast.Expr(value):
+                            if isinstance(value, ast.Constant):
+                                doc = textwrap.dedent(value.value).strip()
+                                assert isinstance(doc, str)
+                process_var_default(context=context, node=kid, doc=doc)
+                # logger.info(f"{dump(kid)=}")
             case ast.Expr():
-                logger.debug(f"unhandled expr: {dump(kid)}")
+                if doc is None:
+                    logger.warning(f"unhandled expr: {dump(kid)}")
+                else:
+                    doc = None
             case _:
                 logger.warning(f"unhandled: {dump(kid)}")
 
 
-def process_var_default(*, context: Context, node: ast.AnnAssign):
+def process_var_default(
+    *,
+    context: Context,
+    node: ast.AnnAssign,
+    doc: Doc,
+):
     target: ast.Name = node.target
     annotation: ast.Name = node.annotation
-    context.defaults[target.id] = annotation.id
+    if doc is not None:
+        context.default_docs[target.id] = doc
+    context.default_types[target.id] = annotation.id
 
 
 def run(*, input: str):
     with open(input) as input_stream:
         text = input_stream.read()
     tree = ast.parse(text)
-    context = Context(defaults={})
+    context = Context(default_docs={}, default_types={})
     process_module(context=context, node=tree)
     script = f"""
         const std = @import("std");
--- ajo/test/basic.zig
+++ ajo/test/basic.zig
@@ -1,9 +1,17 @@
 const Person = struct {
+    /// Could be full name or just preferred name.
     name: str,
+    /// Age in years from birth or manufacture date rounded down.
     age: int,
 };
 const Car = struct {
+    model: str,
+    /// Age in years from birth or manufacture date rounded down.
     age: int,
+};
+const Detail = struct {
+    /// Age in years from birth or manufacture date rounded down.
+    age: float,
 };
 const std = @import("std");
 pub fn main() void {
